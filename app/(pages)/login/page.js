b480ccfadab7b8794de3edfa68d25f24082f636f
Johnson Giang <johnson.giang21@gmail.com>
2024-09-04 21:29:54 -0600

    DON'T REMOVE THE FONTS

---
+++ app/(pages)/login/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "@/app/components/Header";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <div className="bg-red-900 flex items-center justify-center h-screen">
+                <p className="text-6xl font-bold"> hi</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
